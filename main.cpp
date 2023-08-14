15508f8e5960dd7e1aae67742ae195d2e32c5d0c
Pushkar069 <142235803+Pushkar069@users.noreply.github.com>
2023-08-14 19:48:11 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,62 @@
+#include "Buffer/StaticBuffer.h"
+#include "Cache/OpenRelTable.h"
+#include "Disk_Class/Disk.h"
+#include "FrontendInterface/FrontendInterface.h"
+#include <iostream>
+/*int main(int argc, char *argv[]) {
+  Disk disk_run;
+
+  unsigned char buffer[BLOCK_SIZE];
+  Disk::readBlock(buffer, 7000);
+  char message[] = "hello";
+  memcpy(buffer + 20, message, 6);
+  Disk::writeBlock(buffer, 7000);
+
+  unsigned char buffer2[BLOCK_SIZE];
+  char message2[6];
+  Disk::readBlock(buffer2, 7000);
+  memcpy(message2, buffer2 + 20, 6);
+  std::cout << message2;
+
+  return 0;
+}*/
+int main(int argc, char *argv[]) {
+  Disk disk_run;
+
+  // create objects for the relation catalog and attribute catalog
+  RecBuffer relCatBuffer(RELCAT_BLOCK);
+  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
+
+  HeadInfo relCatHeader;
+  HeadInfo attrCatHeader;
+
+  // load the headers of both the blocks into relCatHeader and attrCatHeader.
+  // (we will implement these functions later)
+  relCatBuffer.getHeader(&relCatHeader);
+  attrCatBuffer.getHeader(&attrCatHeader);
+//  int n=SELECT COUNT(*)FROM Students;
+  
+
+  for (int i=0;i<relCatHeader;i++) { // iterate the number of relcat records
+
+    Attribute relCatRecord[RELCAT_NO_ATTRS]; // will store the record from the relation catalog
+
+    relCatBuffer.getRecord(relCatRecord, i);
+
+    printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
+    int numberOfAttr= /*SELECT COUNT(Attribute) FROM Students*/ ;
+
+    for (int j=0;j<k;j++) {
+
+      // declare attrCatRecord and load the attribute catalog entry into it
+
+      if (/* attribute catalog entry corresponds to the current relation */) {
+        const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
+        printf("  %s: %s\n", /* get the attribute name */, attrType);
+      }
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
